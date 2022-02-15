ae9016584b25ff75e7261a8ca126b3a8fb3065d4
Riccardo Guidotti <riccotti@users.noreply.github.com>
2022-02-15 17:56:23 +0100

    Update config.py
    updates for publication

--- experiments/config.py
+++ experiments/config.py
@@ -1,15 +1,7 @@
 import platform
 
-if 'Linux' in platform.platform():
-    path = '/home/riccotti/Documents/Counterfactuals/'
-    path = '/home/riccotti/Documents/CounterfactualExplanations/'
-else:
-    path = '/Users/riccardo/Documents/Research/CounterfactualExplanations/'
-
 path_dataset = path + 'dataset/'
 path_models = path + 'models/'
-# path_results = path + 'results_gpu/'
-# path_cf = path + 'cf_gpu/'
 path_results = path + 'results/'
 path_cf = path + 'cf/'
 path_ae = path + 'aemodels/'
@@ -34,14 +26,3 @@ blackbox_list = ['RF', 'NN', 'SVM', 'DNN', 'DNN2', 'LGBM',
 cfe_list = ['sace-rand', 'sace-tree', 'sace-neig', 'sace-clus', 'sace-feat',
             'dice', 'mace', 'face', 'piece', 'esdce', 'chvae', 'cem', 'icegp', 'macem'
             ]
-
-# Non ri-testare
-# 'sace-rand-1000',
-# 'sace-rand-100000',
-# 'sace-feat-1-20', 'sace-feat-2-20',
-# 'sace-neig-100', 'sace-clus-10',
-# 'sace-tree-4f', 'sace-tree-8f', 'sace-tree-16f',
-
-# Knonw train true /false non cambia nulla quindi mettiamo True
-# search diversity va meglio senza quindi mettiamo False
-# Distanza ok euclidean jaccard
